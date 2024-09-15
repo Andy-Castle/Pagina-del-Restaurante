107c7fd55c3b1a56b9dd202088151cf22f115de3
Andy-Castle <masterchief_and@live.com>
2024-09-15 01:07:10 -0500

    ya acabe el Menu, me falta el contacto

---
+++ src/Menu.js
@@ -0,0 +1,112 @@
+const menu = `      <div class="menu">
+        <h1>Our Meals</h1>
+        <ul class="list">
+          <li>
+            <div class="card">
+              <img
+                src="https://www.allrecipes.com/thmb/MkKfYU4oim55CLp39DDuNMuyThA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/23600-worlds-best-lasagna-armag-4x3-1-b24f9ad518d74090bf197828492c64a6.jpg"
+                alt="Lasagna Photo" />
+              <h3>Lasagna</h3>
+              <p>
+                Layers of pasta, rich Bolognese sauce, creamy béchamel, and a blend
+                of melted cheeses. This Italian classic is baked to perfection for
+                a comforting and hearty meal.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <img
+                src="https://www.allrecipes.com/thmb/bGeuG7WsA8m9N2v7sNLy0cmJZiY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/50223-homemade-crispy-seasoned-french-fries-DDMFS-4x3-4e3f07ae55c9474abc0e4ce601c33eda.jpg"
+                alt="French Fries Photo" />
+              <h3>French Fries</h3>
+              <p>
+                Crispy on the outside and soft on the inside, our golden French
+                fries are seasoned to perfection. Enjoy them as a side or with your
+                favorite dipping sauce.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <img
+                src="https://www.allrecipes.com/thmb/IQcyrbKDWCYFL86MyPpMg5sYEFU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/6770643-basic-baked-spaghetti-ilovecats-1x1-1-6d573ca5eff44566a2819aecbb8b2ecf.jpg"
+                alt="Spaghetti Photo" />
+              <h3>Spaghetti</h3>
+              <p>
+                A plate of perfectly cooked spaghetti, tossed in a savory marinara
+                sauce made from ripe tomatoes, garlic, and fresh herbs. Add a
+                sprinkle of Parmesan cheese to complete this timeless Italian
+                favorite.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <img
+                src="https://www.allrecipes.com/thmb/Q1S1RuecivLTppaVGdIiRIwguaE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/81108-classic-macaroni-salad-mfs-033-23e4b38a9ff046ccb39cc7fc5bbfc7d3.jpg"
+                alt="Macaroni Photo" />
+              <h3>Macaroni</h3>
+              <p>
+                Classic macaroni pasta smothered in a creamy, rich cheese sauce.
+                This dish is comfort food at its finest, sure to satisfy both kids
+                and adults alike.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <img
+                src="https://www.recetasnestle.com.mx/sites/default/files/2023-03/gelato-el-famoso-helado-italiano_4.jpg"
+                alt="Gelato Photo" />
+              <h3>Gelato</h3>
+              <p>
+                Authentic Italian gelato, made with the finest ingredients for a
+                smooth and creamy texture. Choose from a variety of flavors, each
+                offering a sweet and refreshing end to your meal.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <img
+                src="https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/9cffafab1d5436c0695bd417c6200a19/Derivates/400b7cd10996dc118063ff8c2028e2627e28bd36.jpg"
+                alt="Ratatouille Photo" />
+              <h3>Ratatouille</h3>
+              <p>
+                A vibrant medley of fresh vegetables like zucchini, eggplant, bell
+                peppers, and tomatoes, slow-cooked to perfection in a rich tomato
+                sauce. This French dish is both healthy and bursting with flavor.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <img
+                src="https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg?crop=0.503xw:1.00xh;0.234xw,0&resize=1200:*"
+                alt="Crêpes Photo" />
+              <h3>Crêpes</h3>
+              <p>
+                Delicate and thin French crepes, available in both sweet and savory
+                options. Try them filled with fresh fruits and cream or savory
+                fillings like ham and cheese.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="card">
+              <img
+                src="https://thesageapron.com/wp-content/uploads/2022/12/Ravioli-53.jpg"
+                alt="Ravioli Photo" />
+              <h3>Ravioli</h3>
+              <p>
+                Handmade ravioli pasta pockets filled with a blend of cheeses or
+                hearty meats, served in a rich tomato or creamy Alfredo sauce. A
+                comforting and delicious Italian treat.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>`;
+
+export { menu };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import "./style.css";
-// import { home } from "./Home";
+import { home } from "./Home";
+import { menu } from "./Menu";
 
-// const headerButtons = document.querySelectorAll("button");
-// const mainContent = document.getElementById("content");
+const headerButtons = document.querySelectorAll("button");
+const mainContent = document.getElementById("content");
 
-// mainContent.innerHTML = home;
+mainContent.innerHTML = home;
 
-// headerButtons.forEach((button) => {
-//   console.log(button.textContent);
+headerButtons.forEach((button) => {
+  console.log(button.textContent);
 
-//   if (button.textContent == "Home") {
-//     button.addEventListener("click", () => {
-//       mainContent.innerHTML = home;
-//     });
-//   }
-// });
+  if (button.textContent == "Home") {
+    button.addEventListener("click", () => {
+      mainContent.innerHTML = home;
+    });
+  }
+
+  if (button.textContent == "Menu") {
+    button.addEventListener("click", () => {
+      mainContent.innerHTML = menu;
+    });
+  }
+});
